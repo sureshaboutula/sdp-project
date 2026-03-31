3828a146e1a6ff6a674a46cba3ccb7e9203f1793
sureshaboutula <aboutula.suresh@gmail.com>
2026-03-30 20:57:29 -0600

    refactor: extract transform_taxi_data helper for unit testing

--- databricks/notebooks/02_silver_cleansing.py
+++ databricks/notebooks/02_silver_cleansing.py
@@ -8,6 +8,17 @@ schema_bronze = spark.conf.get("schema_bronze")
 schema_silver = spark.conf.get("schema_silver")
 schema_gold = spark.conf.get("schema_gold")
 
+def transform_taxi_data(df):
+    return df.dropDuplicates(["VendorID", "tpep_pickup_datetime", "tpep_dropoff_datetime"]) \
+            .withColumn("fare_amount",F.col("fare_amount").cast("double")) \
+            .withColumn("trip_distance",F.col("trip_distance").cast("double")) \
+            .withColumn("total_amount",F.col("total_amount").cast("double")) \
+            .withColumn("pickup_datetime",F.col("tpep_pickup_datetime").cast("timestamp")) \
+            .withColumn("dropoff_datetime",F.col("tpep_dropoff_datetime").cast("timestamp")) \
+            .withColumn("trip_year",F.year(F.col("tpep_pickup_datetime"))) \
+            .withColumn("trip_month",F.month(F.col("tpep_pickup_datetime")))\
+            .withColumn("trip_hour",F.hour(F.col("tpep_pickup_datetime")))
+
 @dlt.table(
     name= f"sdp_catalog_{env}.{schema_silver}.yellow_taxi_clean",
     comment = "read taxi bronze data into silver layer and clean it",
@@ -19,18 +30,8 @@ schema_gold = spark.conf.get("schema_gold")
 @dlt.expect_or_drop("valid_fare_amount", "fare_amount > 0")
 @dlt.expect_or_drop("vendor_id_not_null", "VendorID is not null")
 def yellow_taxi_data_clean():
-    return(
-        dlt.read(f"sdp_catalog_{env}.{schema_bronze}.yellow_taxi_raw").dropDuplicates(["VendorID", "tpep_pickup_datetime", "tpep_dropoff_datetime"]) \
-            .withColumn("fare_amount",F.col("fare_amount").cast("double")) \
-            .withColumn("trip_distance",F.col("trip_distance").cast("double")) \
-            .withColumn("total_amount",F.col("total_amount").cast("double")) \
-            .withColumn("pickup_datetime",F.col("tpep_pickup_datetime").cast("timestamp")) \
-            .withColumn("dropoff_datetime",F.col("tpep_dropoff_datetime").cast("timestamp")) \
-            .withColumn("trip_year",F.year(F.col("tpep_pickup_datetime"))) \
-            .withColumn("trip_month",F.month(F.col("tpep_pickup_datetime")))\
-            .withColumn("trip_hour",F.hour(F.col("tpep_pickup_datetime")))
-
-    )
+    df = dlt.read(f"sdp_catalog_{env}.{schema_bronze}.yellow_taxi_raw")
+    return transform_taxi_data(df)  
 
 @dlt.table(
     name= f"sdp_catalog_{env}.{schema_silver}.orders_clean",
